da832c81f2c237e06f9ce333b701fb30677e0e7e
Berke FİNCANCI <berkefin@hotmail.com>
2022-11-30 23:15:17 +0300

    FlameCord Kaldırıldı
    FlameCord Sorun sebebi ile waterfall geçiş sağlandı.

--- Hub Paketler/BungeeCord/baslat.bat
+++ Hub Paketler/BungeeCord/baslat.bat
@@ -1,4 +1,4 @@
 @echo off
 title AnkSOFT BungeeCord 
-java -Xms1M -Xmx1G -Dfile.encoding=UTF-8 -jar FlameCord.jar nogui
+java -Xms1M -Xmx1G -Dfile.encoding=UTF-8 -jar waterfall-1.19-507.jar nogui
 pause
