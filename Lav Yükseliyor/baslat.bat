0406ca9589d2df247685741f740ecdb95ba25032
cheraph <64532810+cheraphp@users.noreply.github.com>
2022-12-03 15:00:59 +0300

    Update baslat.bat

--- Lav Yükseliyor/baslat.bat
+++ Lav Yükseliyor/baslat.bat
@@ -1,6 +1,6 @@
 @echo off
 title AnkSOFT
 :start
-java -Xmx1G -Xms1G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:InitiatingHeapOccupancyPercent=20 -Duser.language=en -Duser.region=EN -Dfile.encoding=UTF-8 -jar spigot.jar -o false -nojline --log-count 9 nogui
+java -Xmx1G -Xms1G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:InitiatingHeapOccupancyPercent=20 -Duser.language=en -Duser.region=EN -Dfile.encoding=UTF-8 -jar paper-1.8.8-445.jar -o false -nojline --log-count 9 nogui
 timeout 3 > nul
 goto start
