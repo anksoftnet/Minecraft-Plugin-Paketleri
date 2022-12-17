d201bf6f4bee4a99c68751b44c56bf87791c6792
cheraph <64532810+cheraphp@users.noreply.github.com>
2022-12-17 21:49:12 +0300

    ArenaPvP Eklendi

---
+++ Arena PvP/Start.bat
@@ -0,0 +1,5 @@
+@echo off
+title AnkSOFT - ArenaPvP
+Color 3f
+java -Xms1G -Xmx1G -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -jar Spigot.jar
+PAUSE
